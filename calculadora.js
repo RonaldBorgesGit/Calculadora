5b09ca1784bf80d9a04b508f3e1c5855b51b33a1
RonaldBorgesGit <ronald.borgesgithub@gmail.com>
2022-10-19 07:25:43 -0300

    Calculadora com apenas o front-end

---
+++ calculadora.js
@@ -0,0 +1,54 @@
+const result = document.querySelector("p");
+let um = document.getElementById("um").addEventListener("click", function () {
+    let num = 1
+    result.innerText += num;
+});
+let dois = document.getElementById("dois").addEventListener("click", function () {
+    let num = 2;
+    result.innerText += num;
+});
+let tres = document.getElementById("tres").addEventListener("click", function () {
+    let num = 3;
+    result.innerText += num;
+});
+let quatro = document.getElementById("quatro").addEventListener("click", function () {
+    let num = 4;
+    result.innerText += num;
+});
+let cinco = document.getElementById("cinco").addEventListener("click", function () {
+    let num = 5;
+    result.innerText += num;
+});;
+let seis = document.getElementById("seis").addEventListener("click", function () {
+    let num = 6;
+    result.innerText += num;
+});;
+let sete = document.getElementById("sete").addEventListener("click", function () {
+    let num = 7;
+    result.innerText += num;
+});;
+let oito = document.getElementById("oito").addEventListener("click", function () {
+    let num = 8;
+    result.innerText += num;
+});;
+let nove = document.getElementById("nove").addEventListener("click", function () {
+    let num = 9;
+    result.innerText += num;
+});;
+let zero = document.getElementById("zero").addEventListener("click", function () {
+    let num = 0;
+    result.innerText += num;
+});;
+let c = document.getElementById("c").addEventListener("click", function () {
+    let num = null;
+    result.innerText = num;
+})
+let virgula = document.getElementById("virgula");
+let soma = document.getElementById("soma");
+let subtracao = document.getElementById("subtracao");
+let multiplicacao = document.getElementById("multiplicicao");
+let divisao = document.getElementById("divisao");
+let igual = document.getElementById("igual");
+let porcentagem = document.getElementById("porcentagem");
+let parenteses = document.getElementById("parenteses");
+let pos_neg = document.getElementById("pos_neg");
